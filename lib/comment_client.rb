e6d851ee41438d1f884f3f1f0bca8850af7de182
Rocky Duan <dementrock@gmail.com>
2012-06-19 16:02:30 -0700

    fixed reading service_host

--- lib/comment_client.rb
+++ lib/comment_client.rb
@@ -12,6 +12,10 @@ module CommentClient
       @@service_host = host
     end
 
+    def service_host
+      @@service_host
+    end
+
     def comments_for(commentable)
       response = RestClient.get(url_top_level_comments(commentable)){|response, request, result| response }
       if response.code == 200
